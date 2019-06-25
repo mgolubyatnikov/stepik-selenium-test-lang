1f2db75b04a71008bb9d2c8754c745c3158bab97
Mikhail Golubyatnikov <magolubyatnikov@gmail.com>
2019-06-25 23:02:07 +0300

    initial commit

---
+++ test_items.py
@@ -0,0 +1,8 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_add_to_basket(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector(".btn-add-to-basket")
